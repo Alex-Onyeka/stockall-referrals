89b289d49e7209f34ce1d0411a794c8cbf833e71
Alex Onyeka <alexonyekasm@gmail.com>
2025-06-25 19:35:05 +0100

    Changes

--- lib/pages/auth/auth_base.dart
+++ lib/pages/auth/auth_base.dart
@@ -102,7 +102,7 @@
 //                             //     //   context,
 //                             //     //   MaterialPageRoute(
 //                             //     //     builder: (context) {
-//                             //     //       return SignUp();
+//                             //     //       return AuthBase();
 //                             //     //     },
 //                             //     //   ),
 //                             //     // );
@@ -139,13 +139,306 @@
 //   }
 // }
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class AuthBase extends StatelessWidget {
+// class AuthBase extends StatelessWidget {
+//   const AuthBase({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(body: Center(child: Text('Made it')));
+//   }
+// }
+
+import 'package:flutter/material.dart';
+// import 'package:stockallref/components/email_textfield.dart';
+// import 'package:stockallref/components/general_textfield.dart';
+// import 'package:stockallref/components/main_button.dart';
+// import 'package:stockallref/components/password_textfield.dart';
+// import 'package:stockallref/constants/functions.dart';
+// import 'package:stockallref/constants/images.dart';
+// import 'package:stockallref/supabase/auth_service.dart';
+// import 'package:stockallref/theme/theme.dart';
+
+class AuthBase extends StatefulWidget {
   const AuthBase({super.key});
 
   @override
+  State<AuthBase> createState() => _AuthBaseState();
+}
+
+class _AuthBaseState extends State<AuthBase> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController phone = TextEditingController();
+  TextEditingController state = TextEditingController();
+  TextEditingController name = TextEditingController();
+  TextEditingController confirmPassword =
+      TextEditingController();
+  bool isLoading = false;
+
+  // final supabase = AuthService();
+  // Future<void> createAccount() async {
+  //   try {
+  //     if (email.text.isEmpty ||
+  //         password.text.isEmpty ||
+  //         confirmPassword.text.isEmpty ||
+  //         name.text.isEmpty ||
+  //         state.text.isEmpty ||
+  //         phone.text.isEmpty) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(
+  //           content: Text(
+  //             'All Fields must be set before account can be created',
+  //           ),
+  //         ),
+  //       );
+  //     } else if (!isValidEmail(email.text)) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(content: Text('Email Not Valid')),
+  //       );
+  //     } else if (password.text != confirmPassword.text) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(
+  //           content: Text('Password Does not match'),
+  //         ),
+  //       );
+  //     } else {
+  //       setState(() {
+  //         isLoading = true;
+  //       });
+  //       await supabase.createAccount(
+  //         email: email.text.trim(),
+  //         password: password.text.trim(),
+  //         name: name.text.trim(),
+  //         phone: phone.text.trim(),
+  //         state: state.text.trim(),
+  //       );
+  //       if (mounted) {
+  //         Navigator.of(context).pop();
+  //       }
+  //     }
+  //   } catch (e) {
+  //     setState(() {
+  //       isLoading = false;
+  //     });
+  //     if (mounted) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(
+  //           content: Text(
+  //             'Failed to create Account: ${e.toString().split(',')[0].split(':')[1]}',
+  //           ),
+  //         ),
+  //       );
+  //     }
+  //   }
+  // }
+
+  @override
+  void dispose() {
+    super.dispose();
+    email.dispose();
+    password.dispose();
+    confirmPassword.dispose();
+    name.dispose();
+    phone.dispose();
+    state.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Made it')));
+    return GestureDetector(
+      onTap:
+          () =>
+              FocusManager.instance.primaryFocus?.unfocus(),
+      child: SafeArea(
+        child: Stack(
+          children: [
+            Scaffold(
+              body: Center(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15.0,
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'mainLogoFull',
+                          height: 25,
+                        ),
+                        SizedBox(height: 15),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 30,
+                            vertical: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(5),
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color.fromARGB(
+                                  27,
+                                  0,
+                                  0,
+                                  0,
+                                ),
+                                blurRadius: 10,
+                              ),
+                            ],
+                            color: Colors.white,
+                          ),
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(
+                                  vertical: 5.0,
+                                ),
+                            child: Column(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.center,
+                              spacing: 5,
+                              children: [
+                                Text(
+                                  style: TextStyle(
+                                    fontSize: 27,
+                                    fontWeight:
+                                        FontWeight.w500,
+                                  ),
+                                  'Create Account',
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(
+                                        horizontal: 5.0,
+                                      ),
+                                  child: Text(
+                                    textAlign:
+                                        TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight:
+                                          FontWeight.normal,
+                                      color:
+                                          Colors
+                                              .grey
+                                              .shade500,
+                                    ),
+                                    'Enter All necessary info below to Create an account.',
+                                  ),
+                                ),
+                                // SizedBox(height: 10),
+                                // GeneralTextfield(
+                                //   controller: name,
+                                //   label: 'Enter Name',
+                                // ),
+                                // SizedBox(height: 5),
+                                // TextFieldEmail(
+                                //   controller: email,
+                                // ),
+                                // SizedBox(height: 5),
+                                // TextFieldPassword(
+                                //   controller: password,
+                                //   label: 'Enter Password',
+                                // ),
+                                // SizedBox(height: 5),
+                                // TextFieldPassword(
+                                //   controller:
+                                //       confirmPassword,
+                                //   label: 'Confirm Password',
+                                // ),
+                                // SizedBox(height: 5),
+                                // GeneralTextfield(
+                                //   controller: phone,
+                                //   label:
+                                //       'Enter Phone Number',
+                                //   isPhone: true,
+                                // ),
+                                // SizedBox(height: 5),
+                                // GeneralTextfield(
+                                //   controller: state,
+                                //   label: 'Enter State',
+                                // ),
+                                // SizedBox(height: 10),
+                                // MainButtonBlue(
+                                //   action: () async {
+                                //     await createAccount();
+                                //   },
+                                //   text: 'Sign Up',
+                                // ),
+                                SizedBox(height: 10),
+                                Material(
+                                  color: Colors.transparent,
+                                  child: Ink(
+                                    child: InkWell(
+                                      onTap: () {
+                                        Navigator.of(
+                                          context,
+                                        ).pop();
+                                      },
+                                      child: Container(
+                                        padding:
+                                            EdgeInsets.symmetric(
+                                              vertical: 5,
+                                            ),
+                                        child: Center(
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment
+                                                    .center,
+                                            spacing: 5,
+                                            children: [
+                                              Text(
+                                                style: TextStyle(
+                                                  color:
+                                                      Colors
+                                                          .grey
+                                                          .shade800,
+                                                  fontWeight:
+                                                      FontWeight
+                                                          .normal,
+                                                ),
+                                                'Already Have an Account? ',
+                                              ),
+                                              // Text(
+                                              //   style: TextStyle(
+                                              //     color: secondary(
+                                              //       context,
+                                              //     ),
+                                              //     fontWeight:
+                                              //         FontWeight
+                                              //             .w700,
+                                              //   ),
+                                              //   'Login',
+                                              // ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Visibility(
+              visible: isLoading,
+              child: Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
