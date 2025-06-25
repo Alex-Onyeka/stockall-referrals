c7bf6d3ae812a1429575be591b3f3d5f4efef384
Alex Onyeka <alexonyekasm@gmail.com>
2025-06-25 16:55:45 +0100

    Changes

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,9 +1,9 @@
-String mainLogoFull = 'assets/logos/logo_full.png';
+// String mainLogoFull = 'assets/logos/logo_full.png';
 
-String mainLogoIcon = 'assets/logos/logo_icon.png';
+// String mainLogoIcon = 'assets/logos/logo_icon.png';
 
-String logoIconWhite = 'assets/logos/icon_white.png';
+// String logoIconWhite = 'assets/logos/icon_white.png';
 
-String appMockUp = 'assets/logos/app_mockup.png';
+// String appMockUp = 'assets/logos/app_mockup.png';
 
-String splashImage = 'assets/logos/splash_image2.png';
+// String splashImage = 'assets/logos/splash_image2.png';
